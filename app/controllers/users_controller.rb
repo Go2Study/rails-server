4f00f29c55c078ebd02fd82d1f172d1ed33e7f04
Ashish Kumar <ashish@ashish.co>
2015-11-10 23:12:33 +0100

    Show error when user record not found

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,29 @@
 class UsersController < ApplicationController
-  
+
   before_action :load_user,
     only: [:show, :update]
-  
+
   ### ACTIONS ###
-  
+
   def index
     @users = User.all
     render json: @users, except: [:id, :created_at, :updated_at]
   end
-  
+
   def create
     @user = User.new user_create_params
-    
+
     if @user.save
       render json: @user
     else
       render text: "fail"
     end
   end
-  
+
   def show
     render json: @user
   end
-  
+
   def update
     if @user.update_attributes user_update_params
       render json: @user
@@ -31,21 +31,32 @@ class UsersController < ApplicationController
       render text: "fail"
     end
   end
-  
+
   ### PRIVATE ###
-  
+
   private
-  
+
   def user_create_params
     params.permit(:pcn, :first_name, :last_name, :display_name, :mail, :photo, :ip_address)
   end
-  
+
   def user_update_params
     params.permit(:display_name, :mail, :photo, :ip_address)
   end
-  
+
   def load_user
-    @user = User.find params[:id]
+    begin
+      @user = User.find params[:id]
+    rescue ActiveRecord::RecordNotFound
+      error = {
+        error: {
+          code: 404,
+          message: 'Not Found'
+        }
+      }
+
+      render json: error.to_json, status: 404
+    end
   end
-  
+
 end
